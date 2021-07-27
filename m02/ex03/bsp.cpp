4c083e3c63a8f248ed9d1b0e464f6e69ca124c4a
scilla <scilla@student.42.fr>
2021-07-27 19:33:07 +0200

    bsp equal

--- m02/ex03/bsp.cpp
+++ m02/ex03/bsp.cpp
@@ -13,8 +13,8 @@ bool bsp( Point const a, Point const b, Point const c, Point const point)
     d1 = sign(point, a, b);
     d2 = sign(point, b, c);
     d3 = sign(point, c, a);
-    has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
-    has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+    has_neg = (d1 <= 0) || (d2 <= 0) || (d3 <= 0);
+    has_pos = (d1 >= 0) || (d2 >= 0) || (d3 >= 0);
 
     return !(has_neg && has_pos);
 }
