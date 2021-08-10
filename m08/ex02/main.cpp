4a7f3c6c02d9d44e36fe0cf8770cb7544cbb5390
scilla <scilla@student.42.fr>
2021-08-10 15:30:38 +0200

    up

--- m08/ex02/main.cpp
+++ m08/ex02/main.cpp
@@ -4,12 +4,16 @@
 #include <vector>
 #include <deque>
 
-int main(void) {
-
+int main(void)
+{
 	std::deque<int> mydeque (3,100);          // deque with 3 elements
+	std::stack<int> mystack;          // deque with 3 elements
+	mystack.push(11);
 	std::vector<int> myvector (2,200);        // vector with 2 elements
 	
-	MutantStack<int, std::vector<int> > s(myvector);
+	MutantStack<int> s(mystack);
+	MutantStack<int> t(myvector);
+	MutantStack<int> u(mydeque);
 
 	s.push(10);
 	s.push(20);
