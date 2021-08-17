d5778c6731cc4241fa2545a2921b209d6e26666c
scilla <scilla@student.42.fr>
2021-08-17 15:27:09 +0200

    main up

--- m07/ex02/main.cpp
+++ m07/ex02/main.cpp
@@ -1,5 +1,6 @@
 #include "Array.hpp"
 #include "Test.hpp"
+#define MAX_VAL 750
 
 int	main()
 {
@@ -290,4 +291,53 @@ int	main()
 		std::cout << b[1] << std::endl;
 		std::cout << c[1] << std::endl;
 	}
+	
+	std::cout << std::endl << " ##### default ##### " << std::endl << std::endl;
+
+	Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;
+	return (0);
 }
